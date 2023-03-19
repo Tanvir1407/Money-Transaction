ceb8d20d86a528fe0cea2f2dca31be01cd4a23b5
MD. Tanvir <mt14050@gmail.com>
2023-03-19 09:18:48 +0600

    create API

---
+++ src/Features/TransactiosAPI.js
@@ -0,0 +1,19 @@
+import axios from "../utilitis/axios"
+
+
+export const getTransactions = async () => {
+    const response = await axios.get("/transactions")
+    return response.data;
+}
+export const addTransactions = async (data) => {
+    const response = await axios.post("/transactions", data)
+    return response.data;
+}
+export const updateTransactions = async (id,data) => {
+    const response = await axios.put(`/transactions/${id}`, data)
+    return response.data;
+}
+export const deleteTransactions = async (id) => {
+    const response = await axios.delete(`/transactions/${id}`)
+    return response.data;
+}
